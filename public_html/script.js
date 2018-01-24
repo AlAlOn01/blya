5bea84f1b5a25087e56e89e28f067e8dfcc9bfb3
AlAlOn01 <35781352+AlAlOn01@users.noreply.github.com>
2018-01-25 00:29:49 +0300

    Update script.js

--- public_html/script.js
+++ public_html/script.js
@@ -1,15 +1,13 @@
 const app = new Vue({
 	el: '#app',
 	data: {
-		products: []
+		posts: []
 	},
 	created () {
 		fetch('https://legal-consult.online/blya/api/test/')
-		.then (response => response.json())
+		.then (response => response.posts)
 		.then (json => {
-			this.products = json.products
-			// Тут должно быть не json.products, а путь к списку posts в приходящем json, то бишь json.posts
-            // Ну и переименуй products в posts наверху, у нас таки посты, а не продукты))
+			this.posts = json.posts
 		})
 	}
 })
