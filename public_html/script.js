55bb4ea0d392ea6733506a2a2d61b6f9ffc80cb1
van4oza <chezhik@gmail.com>
2018-01-25 00:04:17 +0300

    Доступ к списку в приходящем json от api

--- public_html/script.js
+++ public_html/script.js
@@ -8,6 +8,8 @@ const app = new Vue({
 		.then (response => response.json())
 		.then (json => {
 			this.products = json.products
+			// Тут должно быть не json.products, а путь к списку posts в приходящем json, то бишь json.posts
+            // Ну и переименуй products в posts наверху, у нас таки посты, а не продукты))
 		})
 	}
 })
